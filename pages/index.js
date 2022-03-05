ee8d56064b3c37a40d77fab5077b778ab7ef9c9c
CS <65098478+javascriptseo@users.noreply.github.com>
2022-03-04 17:51:28 -0800

    app setup

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Footer from '../comps/Footer'
+import Navbar from '../comps/Navbar'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Homepage</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+      <Link href="/ninjas/">
+        <a>See Ninja Listing</a>
+      </Link>
+      <Footer />
+    </div>
+  )
+}
